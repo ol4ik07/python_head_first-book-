9223cd8072c7de96fe0a79fa9fdb54cfebb637ef
OlgaPC <olenka89@list.ru>
2018-08-13 21:49:36 +0300

    q

--- vsearch4web_redirect.py
+++ vsearch4web_redirect.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from vsearch import search4letters
 
 app = Flask(__name__)
+
+@app.route('/')
+def hello()-> '302':
+    return redirect('/entry')
 
 @app.route('/search4', methods=['POST'])
 def do_search()-> 'html':
@@ -10,10 +14,8 @@ def do_search()-> 'html':
     title='Here are your results:'
     results=str(search4letters(phrase,letters))
     return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results,)
-@app.route('/')
+
 @app.route('/entry')
 def entry_page()-> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
-
-if __name__=='__main__':
-    app.run(debug=True)
+app.run(debug=True)
