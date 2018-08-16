bba08eb8420a9782cb11bfc1902d6587c1d662fd
ol4ik07 <olenka89@list.ru>
2018-08-16 21:57:16 +0300

    install module for mac.test

--- vsearch4web.py
+++ vsearch4web.py
@@ -16,10 +16,12 @@ def do_search()-> 'html':
     results=str(search4letters(phrase,letters))
     log_request(request,results)
     return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results,)
+
 @app.route('/')
 @app.route('/entry')
 def entry_page()-> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
 @app.route('/viewlog')
 def view_the_log() -> str:
     with open('vsearch.log') as log:
