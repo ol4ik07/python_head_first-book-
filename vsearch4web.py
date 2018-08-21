8a344b23f18af0633f9185259ea2aab431554faa
ol4ik07 <olenka89@list.ru>
2018-08-21 23:47:02 +0300

    connect database, change log

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+from dbcm import UseDataBase
 
 app = Flask(__name__)
+app.config['dbconfig']={'host':'127.0.0.1', 'user':'vsearch', 'password':'vsearch','database':'vsearchlogdb',}
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr,req.user_agent, res, file=log, sep='|')
+    with UseDataBase(app.config['dbconfig']) as cursor:
+
+        _SQL="""insert into log (phrase,letters,ip,browser_string,results) values (%s,%s,%s,%s,%s)"""
+        cursor.execute(_SQL,(req.form['phrase'], req.form['letters'],req.remote_addr, req.user_agent.browser,res,))
+
 
 @app.route('/search4', methods=['POST'])
 def do_search()-> 'html':
@@ -25,12 +30,11 @@ def entry_page()-> 'html':
 @app.route('/viewlog')
 def view_the_log() -> str:
     contents=[]
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|'):
-                contents[-1].append(escape(item))
-    titles=('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    with UseDataBase(app.config['dbconfig']) as cursor:
+        _SQL="""select phrase, letters, ip, browser_string, results from log"""
+        cursor.execute(_SQL)
+        contents=cursor.fetchall()
+    titles=('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
 
     return render_template('viewlog.html', the_title='View log', the_row_titles=titles,the_data=contents,)
 
