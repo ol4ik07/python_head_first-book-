62f17c88c57d7061082f318490d8bd75d8a26e9e
ol4ik07 <olenka89@list.ru>
2018-08-22 21:00:29 +0300

    Remove all files

--- marvin.py
+++
@@ -1,4 +0,0 @@
-paranoid_androin="Marvin"
-letters=list(paranoid_androin)
-for char in letters:
-    print('\t',char)
--- nested_lists.py
+++
@@ -1,6 +0,0 @@
-students = {}
-for _ in range(int(input())):
-    name = input()
-    score = float(input())
-    students[name] = score
-    print (students)
